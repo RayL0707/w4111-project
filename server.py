db9fceff3c5358e606f88972cf011130bed9db9c
Rong Lei <rl2849@columbia.edu>
2016-11-07 13:21:31 -0500

    change ip

--- server.py
+++ server.py
@@ -495,7 +495,7 @@ if __name__ == "__main__":
   @click.command()
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('HOST', default='13.72.103.18')
   @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
     """
