a779a03a47f8f9ac91a1307cd16798906735227a
Rong Lei <rl2849@columbia.edu>
2016-11-09 14:28:04 -0500

    port 8111

--- server.py
+++ server.py
@@ -543,7 +543,7 @@ if __name__ == "__main__":
   @click.option('--debug', is_flag=True)
   @click.option('--threaded', is_flag=True)
   @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8118, type=int)
+  @click.argument('PORT', default=8111, type=int)
   def run(debug, threaded, host, port):
     """
     This function handles command line parameters.
